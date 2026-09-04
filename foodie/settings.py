b8b431b2c1f8df0e18fb128240d81a1e11fdd654
Harshita Soni <soniharshita8126@gamil.com>
2026-09-04 13:13:00 +0530

    Setup Django frontend structure

--- foodie/settings.py
+++ foodie/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'foodie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
