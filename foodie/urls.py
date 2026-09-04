f755e93ab4ca51f0aca925382b64874702e5a717
Harshita Soni <soniharshita8126@gamil.com>
2026-09-04 22:52:01 +0530

    Added login and Signup features with forget password ans reset password

--- foodie/urls.py
+++ foodie/urls.py
@@ -4,10 +4,53 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
+
+def restaurants(request):
+    return render(request, "restaurants.html")
+
+
+def menu(request):
+    return render(request, "menu.html")
+
+
+def offers(request):
+    return render(request, "offers.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+def login(request):
+    return render(request, "login.html")
+
+
+def admin_login(request):
+    return render(request, "admin-login.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
+
+def forgot_password(request):
+    return render(request, "forgot-password.html")
+
+
+def reset_password(request):
+    return render(request, "reset-password.html")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
+    path("admin/", admin.site.urls),
+
+    path("", home, name="home"),
+    path("restaurants/", restaurants, name="restaurants"),
+    path("menu/", menu, name="menu"),
+    path("offers/", offers, name="offers"),
+    path("about/", about, name="about"),
+    path("login/", login, name="login"),
+    path("admin-login/", admin_login, name="admin_login"),
+    path("signup/", signup, name="signup"),
+    path("forgot-password/", forgot_password, name="forgot_password"),
+    path("reset-password/", reset_password, name="reset_password"),
 ]
